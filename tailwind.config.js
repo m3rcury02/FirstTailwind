311dd460f1b2d805103ce2f6d4d6f784d8c99560
m3rcury02 <gunal2002@gmail.com>
2023-08-07 00:57:22 +0530

    Dark mode logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,6 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    screens:{
-      sm:'480px',
-      md:'768px',
-      lg:'976px',
-      xl:'1440px'
-    },
     extend: {
       colors:{
         brightRed: 'hsl(12,88%,59%)'
